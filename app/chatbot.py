b300da84ea9d4d91d7708048dcf9a29635942b70
Smarika <smarikakc96@gmail.com>
2026-06-10 09:11:40 +0545

    feat: add conversation memory to RAG chain closes #12

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,4 +1,4 @@
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from services.llm_service import get_llm
@@ -7,23 +7,22 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 def create_rag_chain(retriever):
-    prompt = ChatPromptTemplate.from_template("""
-    You are a helpful assistant. Answer the question based only on the context below.
-    If the answer is not in the context, say "I don't know".
-    
-    Context: {context}
-    
-    Question: {question}
-    
-    Answer:
-    """)
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", """You are a helpful assistant. Answer the question based only on the context below.
+        If the answer is not in the context, say "I don't know"
+        
+        Context: {context}"""),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{question}")
+    ])
     
     llm = get_llm()
     
     rag_chain = (
         {
-            "context": retriever | format_docs,
-            "question": RunnablePassthrough()
+            "context": (lambda x: x["question"]) | retriever | format_docs,
+            "question": lambda x: x["question"],
+            "chat_history": lambda x: x["chat_history"]
         }
         | prompt
         | llm
--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from services.document_service import load_and_chunk_documents
 from services.embedding_service import create_embeddings
 from services.retriever_service import create_retriever
 from app.chatbot import create_rag_chain
+from langchain_core.messages import HumanMessage, AIMessage
 
 app=FastAPI()
+
+chat_history = []
+
 
 chunks = load_and_chunk_documents()
 vector_store = create_embeddings(chunks)
@@ -17,5 +21,9 @@ class ChatRequest(BaseModel):
 
 @app.post("/chat")
 def chat(request:ChatRequest):
-    response = rag_chain.invoke(request.question)
+    response = rag_chain.invoke({
+        "question":request.question,
+        "chat_history":chat_history})
+    chat_history.append(HumanMessage(content=request.question))
+    chat_history.append(AIMessage(content=response))
     return {"answer":response}
